5d4e9bcca73b5171c24335fb2e897fdf3eb73cc6
kronrraci <kronrraci@gmail.com>
2026-06-24 20:50:43 +0200

    moduel-21

---
+++ moduel-21/js/main.js
@@ -0,0 +1,43 @@
+console.log("Goodbye")
+
+// var - variable
+// x - emri i variables
+// 5 - vlera e variables
+var x = 5; //number
+
+//var 123 - 5 - gabim, nuk lejohet emertimi vetem me numra
+
+
+var a = 50 + 50
+console.log(a)
+
+var h = 20
+var c = 30
+var d = b + c
+console.log(d)
+
+var e = 40;
+var f = 10;
+var g = e - f;
+console.log(g);
+
+var h = 100;
+var i = 5;
+var j = 100 / 5;
+console.log(j);
+
+// moduli - mbetja
+var k = 10;
+var l = 4;
+var m = k % 1;
+console.log(m);
+
+//rritja per nje numer
+var n = 99;
+n++;
+console.log(n);
+
+//zbritja per nje numer
+var o = 101;
+o--;
+console.log(o);
